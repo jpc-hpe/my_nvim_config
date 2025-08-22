0c56d543c4f5889e381d0470b23bd054d6ee0792
Juan Postlbauer <juan.postlbauer@hpe.com>
2025-08-22 09:36:25 +0200

    Switch back to github version of chitshit

--- lua/plugins/chitshit.lua
+++ lua/plugins/chitshit.lua
@@ -1,6 +1,6 @@
 return {
-  -- "jpc-hpe/chitshit.nvim",
-  dir = "/mnt/wsl/extradata/git/jpc-hpe/chitshit.nvim",
+  "jpc-hpe/chitshit.nvim",
+  -- dir = "/mnt/wsl/extradata/git/jpc-hpe/chitshit.nvim",
   -- Leave one (and only one) of the 2 lines above uncommented to switch between
   -- the locally developed copy and the one from GitHub.
   opts = {},
