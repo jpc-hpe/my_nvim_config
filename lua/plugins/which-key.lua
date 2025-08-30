a84903ce4ebb8c71ab217963e6c86ea986417fcf
Juan Postlbauer <juan.postlbauer@hpe.com>
2025-08-30 16:12:19 +0200

    Reverse wrong which key scroll directions

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,8 +3,8 @@ return {
   optional = true,
   opts = {
     keys = {
-      scroll_down = "<S-Up>", -- binding to scroll down inside the popup
-      scroll_up = "<S-Down>", -- binding to scroll up inside the popup
+      scroll_up = "<S-Up>", -- binding to scroll down inside the popup
+      scroll_down = "<S-Down>", -- binding to scroll up inside the popup
     },
   },
   keys = {
